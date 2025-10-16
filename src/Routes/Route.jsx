05a854c3bb5ba23e8eea3564c3b0683d35d08608
Tariful Hasan <tarif305@gmail.com>
2025-10-16 22:18:01 +0600

    routes added

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,16 +1,26 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import Register from "../Components/Register";
+import Home from "../Components/Home";
+import Login from "../Components/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     children: [
+        {
+            index: true,
+            Component: Home
+        },
       {
         path: "register",
         Component: Register,
       },
+      {
+        path: 'login',
+        Component: Login
+      }
     ],
   },
 ]);
